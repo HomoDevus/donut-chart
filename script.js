fef4c24630c98974ef63d7ee84a76b818802adfb
ItIsEternity <lavrovslava207@gmail.com>
2023-08-17 20:23:47 +0300

    code style fixes

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function getRandomNumberInRange(a, b) {
 }
 
 function getRandomItems(array, n) {
-    const shuffledArray = array.slice(); // Copy the array to avoid modifying the original
+    const shuffledArray = array.slice();
 
     for (let i = shuffledArray.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -57,33 +57,37 @@ function createDonutChart(data) {
     svg.innerHTML = ''
 
     data.forEach(({ value, radiusScale }, index) => {
-        endAngle = startAngle + (360 * value) / totalValue;
-
         const radius = Math.round(baseRadius * radiusScale)
         const color = randomCoolors[index]
-
-        const startX = centerX + radius * Math.cos((startAngle * Math.PI) / 180);
-        const startY = centerY + radius * Math.sin((startAngle * Math.PI) / 180);
-
-        const endX = centerX + radius * Math.cos((endAngle * Math.PI) / 180);
-        const endY = centerY + radius * Math.sin((endAngle * Math.PI) / 180);
-
-        const innerStartX = centerX + innerRadius * Math.cos((endAngle * Math.PI) / 180);
-        const innerStartY = centerY + innerRadius * Math.sin((endAngle * Math.PI) / 180);
-
-        const innerEndX = centerX + innerRadius * Math.cos((startAngle * Math.PI) / 180);
-        const innerEndY = centerY + innerRadius * Math.sin((startAngle * Math.PI) / 180);
-
-        const largeArcFlag = value / totalValue <= 0.5 ? 0 : 1;
 
         const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
 
         if (data.length === 1) {
+            // If there is one section just draw one complete donut
             path.setAttribute("d", `M ${centerX} ${centerY - radius}` +
                 ` A ${radius} ${radius} 0 1 1 ${centerX - 0.1} ${centerY - radius}` +
                 ` L ${centerX - 0.1} ${centerY - innerRadius}` +
                 ` A ${innerRadius} ${innerRadius} 0 1 0 ${centerX} ${centerY - innerRadius} Z`);
         } else {
+            endAngle = startAngle + (360 * value) / totalValue;
+
+            // Start and end of outer radian
+            const startX = centerX + radius * Math.cos((startAngle * Math.PI) / 180);
+            const startY = centerY + radius * Math.sin((startAngle * Math.PI) / 180);
+
+            const endX = centerX + radius * Math.cos((endAngle * Math.PI) / 180);
+            const endY = centerY + radius * Math.sin((endAngle * Math.PI) / 180);
+
+            // Start and end of inner radian
+            const innerStartX = centerX + innerRadius * Math.cos((endAngle * Math.PI) / 180);
+            const innerStartY = centerY + innerRadius * Math.sin((endAngle * Math.PI) / 180);
+
+            const innerEndX = centerX + innerRadius * Math.cos((startAngle * Math.PI) / 180);
+            const innerEndY = centerY + innerRadius * Math.sin((startAngle * Math.PI) / 180);
+
+            // If section takes more than half of chart set flag to 1 otherwise to 0
+            const largeArcFlag = value / totalValue <= 0.5 ? 0 : 1;
+
             path.setAttribute(
                 "d",
                 `M ${startX} ${startY} A ${radius} ${radius} 0 ${largeArcFlag} 1 ${endX} ${endY}` +
